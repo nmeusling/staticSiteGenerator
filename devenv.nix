1726d8cff05e707cba2e9e0759f01e8b282bde46
nmeusling <15682036+nmeusling@users.noreply.github.com>
2025-01-21 10:47:02 -0500

    Initial commit for static site generator

---
+++ devenv.nix
@@ -0,0 +1,27 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.poetryPlugins.poetry-plugin-export
+  ];
+
+
+  # https://devenv.sh/languages/
+  languages = {
+    python = {
+      enable = true;
+      version = "3.12";
+      poetry = {
+        enable = true;
+        activate.enable = true;
+      };
+    };
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
