b11129fbf7997c018aff419562273f643cba1e4f
nmeusling <15682036+nmeusling@users.noreply.github.com>
2025-01-27 18:53:28 -0500

    feat: add link and image extraction

--- src/markdown_tools.py
+++ src/markdown_tools.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextType, TextNode
 
 
@@ -32,7 +33,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return new_nodes
 
 def extract_markdown_images(text):
-    alt_text = ""
-    url = ""
+    pattern = r"!\[(.*?)\]\((.*?)\)"
+    return re.findall(pattern, text) 
 
-    return alt_text, url    
+def extract_markdown_links(text):
+    pattern = r"[^!]\[(.*?)\]\((.*?)\)"
+    return re.findall(pattern, text)
--- src/test_markdown_tools.py
+++ src/test_markdown_tools.py
@@ -3,7 +3,7 @@ import unittest
 
 from textnode import TextNode, TextType
 
-from markdown_tools import split_nodes_delimiter, extract_markdown_images
+from markdown_tools import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestSplitNodesDelimiter(unittest.TestCase):
     def test_single_node_split(self):
@@ -104,4 +104,35 @@ class TestExtractMarkdownImages(unittest.TestCase):
         text = "This is text with no images"
         result = extract_markdown_images(text) 
         expected = []
+        self.assertEqual(result, expected)
+    
+    def test_link_not_extracted_as_image(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) [other](https://i.imgur.com/fJRm4Vk.jpeg)"
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        result = extract_markdown_images(text)
+        self.assertEqual(result, expected)
+
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_single_link(self):
+        text = "This is text with a [link text](www.source.url)"
+        result = extract_markdown_links(text)
+        expected = [("link text", "www.source.url")]
+        self.assertEqual(result, expected)
+
+    def test_multiple_images(self):
+        text = "This is text with a [two links](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        result = extract_markdown_links(text) 
+        expected = [("two links", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(result, expected)
+
+    def test_no_images(self):
+        text = "This is text with no links"
+        result = extract_markdown_links(text) 
+        expected = []
+        self.assertEqual(result, expected)
+    
+    def test_image_not_extracted_as_link(self):
+        text = "This is text with a [rick roll](www.test.com) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) [other](www.other.com)"
+        expected = [("rick roll", "www.test.com"), ("other", "www.other.com")]
+        result = extract_markdown_links(text)
         self.assertEqual(result, expected)
