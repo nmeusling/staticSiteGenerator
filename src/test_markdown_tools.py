5f1da5743a5ba33feffb7a4950eed45be0fb48b2
nmeusling <15682036+nmeusling@users.noreply.github.com>
2025-01-28 17:40:37 -0500

    WIP split link

--- src/test_markdown_tools.py
+++ src/test_markdown_tools.py
@@ -136,3 +136,17 @@ class TestExtractMarkdownLinks(unittest.TestCase):
         expected = [("rick roll", "www.test.com"), ("other", "www.other.com")]
         result = extract_markdown_links(text)
         self.assertEqual(result, expected)
+
+class TestSplitNodesLink(self):
+    node = TextNode(
+        "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_link([node])
+    expected = [
+        TextNode("This is text with a link ", TextType.TEXT),
+        TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+        TextNode(" and ", TextType.TEXT),
+        TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
+    ]
+    self.assertEqual(new_nodes, expected)
